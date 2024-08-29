ff771e498737d16f975c01dbb49eb580b1950292
Omar <omarkhaled1752003@yahoo.com>
2024-08-29 09:32:02 +0300

    url for update task api view

--- tasks_api/urls.py
+++ tasks_api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("tasks-list", views.tasks_list, name="tasks-list"),
     path("create-task", views.create_task, name="create-task"),
     path("view-task/<int:pk>", views.view_task, name="view-task"),
+    path("update-task/<int:pk>", views.update_task, name="update-task"),
 ]
