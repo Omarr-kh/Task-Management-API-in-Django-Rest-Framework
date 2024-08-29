2691bad26e1266c4fcf7a68e9640f3758c6a62c6
Omar <omarkhaled1752003@yahoo.com>
2024-08-29 09:14:37 +0300

    api view for task creation

--- tasks_api/views.py
+++ tasks_api/views.py
@@ -14,3 +14,13 @@ def tasks_list(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+
+@api_view(["POST"])
+def create_task(request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
