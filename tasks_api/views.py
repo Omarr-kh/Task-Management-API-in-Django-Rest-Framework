2ece43531947bb9d0972817a734ced5cd5b5fc98
Omar <omarkhaled1752003@yahoo.com>
2024-08-29 09:01:42 +0300

    view to testing

--- tasks_api/views.py
+++ tasks_api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["GET"])
+def tasks_list(request):
+    data = {
+        "name": "omar",
+        "age": 21,
+        "status": "single",
+    }
+    return Response(data)
