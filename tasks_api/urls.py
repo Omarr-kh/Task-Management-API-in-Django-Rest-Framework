5e5dc17165b6b2407e700e056b5d6a51c72e321f
Omar <omarkhaled1752003@yahoo.com>
2024-08-29 09:17:19 +0300

    view a task url

--- tasks_api/urls.py
+++ tasks_api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("tasks-list", views.tasks_list, name="tasks-list"),
     path("create-task", views.create_task, name="create-task"),
+    path("view-task/<int:pk>", views.view_task, name="view-task"),
 ]
