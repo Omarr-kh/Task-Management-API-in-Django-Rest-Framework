2723ee185cc05c25231dcfb9c5c5e2655a285765
Omar <omarkhaled1752003@yahoo.com>
2024-08-29 09:01:00 +0300

    Task model

--- tasks_api/models.py
+++ tasks_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    # each task has to be linked with a user
+    # all tasks are deleted if the user is deleted
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField(blank=False)
+    status = models.BooleanField(default=False, verbose_name="completed")
