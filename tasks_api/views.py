47d89ac37cddb960ee2c042082fd375befbbf1a9
Omar <omarkhaled1752003@yahoo.com>
2024-08-29 09:06:36 +0300

    api view to list tasks

--- tasks_api/views.py
+++ tasks_api/views.py
@@ -5,12 +5,12 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Task
+from .serializers import TaskSerializer
+
 
 @api_view(["GET"])
 def tasks_list(request):
-    data = {
-        "name": "omar",
-        "age": 21,
-        "status": "single",
-    }
-    return Response(data)
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
